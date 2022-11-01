6874bff9c66d5a6e8e5e1dd4c4ed180ade1484fa
Kozerema <kozeremavika@gmail.com>
2022-11-01 16:00:46 +0200

    hw1_js

---
+++ lesson1/hw_1/main.js
@@ -0,0 +1,51 @@
+"use strict";
+let a = 'hello';
+console.log(a);
+let b = 'owu';
+console.log(b);
+let c = 'com';
+console.log(c);
+let d = 'ua';
+console.log(d);
+
+let one = 1;
+console.log(one);
+let two = 10;
+console.log(two);
+let three = -999;
+console.log(three);
+let four = 123;
+console.log(four);
+const PI = 3.14;
+console.log(PI);
+let five = 2.7
+console.log(five);
+let six = 16;
+console.log(six);
+
+let s1 = 5 !== '5';
+console.log(s1);
+let s2 = 6 - 3 > 15;
+console.log(s2);
+
+let firstName='Вікторія';
+let middleName='Андріївна';
+let lastName='Козерема';
+//конкатинація
+console.log(`${lastName} ${firstName} ${middleName}`); //templete string
+// console.log(lastName+''+firstName+''+middleName) regular string
+
+a=100;
+console.log(typeof a);
+b = '100'
+console.log(typeof b);
+c = true;
+console.log(typeof c);
+// console.log((100+200+'!!')) стрінга
+// console.log(+'125'); число
+// console.log(!!0);false
+// console.log(!!'');false
+// console.log(!!null); false
+// console.log(!!undefined); false
+// console.log(!!NaN); false
+
